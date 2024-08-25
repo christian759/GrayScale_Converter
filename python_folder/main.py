d5a6ac016ba914d7fd5c5eab661131037db5e991
CEO1 <christian4onos@outlook.com>
2024-08-25 18:37:51 +0100

    done

---
+++ python_folder/main.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import os
+
+folder_path: str = "python_folder/picture_folder"
+images: list = []
+imagePaths: list[str] = []
+
+for img in os.listdir(folder_path):
+    imagepath = os.path.join(folder_path, img)
+    image = Image.open(imagepath)
+    images.append(image)
+    imagePaths.append(imagepath)
+
+j: int = 0
+print(len(imagePaths))
+for i in range(0, len(imagePaths)):
+    image = Image.open(imagePaths[i])
+    image = image.convert("L")
+    image.save("grayscale_image"+str(i)+".jpg")
